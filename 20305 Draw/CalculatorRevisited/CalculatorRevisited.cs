6c0a9eb5cda49fcd3a42d93ff6348bb97f71d7b5
Kostia Kolomiiets <kostiakolomiiets@gmail.com>
2024-03-05 11:21:57 +0100

    0305

---
+++ 20305 Draw/CalculatorRevisited/CalculatorRevisited.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Dnes bude vasim ukolem vytvorit formularovou reprezentaci kalkulacky. Priblizny vzhled si nakreslime na tabuli
+ * (Pokud jsem to nenakreslil, pripomente mi to prosim!)
+ * Inspirujte se kalkulackou ve Windows.
+ * Pozadovane prvky/funkcionality:
+ * - Tlacitka pro kazde z cisel 0-9
+ * - Tlacitka pro operace +, -, *, a /
+ * - Tlacitko pro = (vypsani vysledku)
+ * - Textbox, do ktereho se propisou cisla/operace pri stisku jejich tlacitek
+ * - Textbox s vysledkem operace (mozne sloucit s predeslym, nechavam na vas)
+ * - Tlacitko pro vymazani textu v textboxu s cisly a operaci
+ * 
+ * Volitelne prvky/funkcionality:
+ * - Tlacitko pro mazani cisel a operaci v textboxu zprava vzdy po jednom znaku
+ * - Pokud mam vypsany vysledek a hned po tom stisknu tlacitko nejake operace, vysledek se vezme jako prvni cislo a
+ *   rovnou mohu po zadani operace zadat druhe cislo
+ * - Moznost ulozeni vysledku do nekolika preddefinovanych labelu/neinteraktivnich textboxu (treba kombinaci comboboxu a tlacitka, kde
+ *   v comboboxu vyberu do ktereho labelu/textboxu se mi to ulozi a tlacitkem provedu ulozeni)
+ *   + pridat tlacitko pro kazdy label/neint. textbox, kterym ulozeny vysledek pouziju jako cislo do vypoctu
+ * - Pridani mocnin/odmocnin atd. (napr. pomoci dalsich tlacitek, ktere rovnou misto daneho cisla daji jeho (popr. zaokrouhlenou) odmocninu apod.)
+ * - Cokoliv dalsiho vas napadne! :)
+ * 
+ * Snazte se o to, aby byla kalkulacka v ramci moznosti hezka, a aby bylo jeji ovladani intuitivni a aby mel kazdy prvek v okne dobre vyuziti
+ */
+
+namespace CalculatorRevisited
+{
+    public partial class CalculatorRevisited : Form
+    {
+        Graphics graphics;
+        int x = -1, y = -1;
+        bool move = false;
+        Pen pen;
+        public CalculatorRevisited()
+        {
+            InitializeComponent();
+            graphics = panel1.CreateGraphics();
+            graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            pen = new Pen(Color.Black, 5);
+        }
+
+        private void CalculatorRevisited_Paint(object sender, PaintEventArgs e)
+        {
+            graphics.DrawLine(Pens.Red, 10, 10, 100, 100);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            //graphics = e.Graphics;
+            // graphics.DrawLine(Pens.Red, 10, 10, 100, 100);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            pen.Color = p.BackColor;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(move && x != -1 && y != -1)
+            {
+                graphics.DrawLine(pen, new Point(x, y), e.Location);
+                x = e.X;
+                y = e.Y;
+            }
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            move = false;
+            x = -1;
+            y = -1;
+        }
+
+        private void CalculatorRevisited_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            move = true;
+            x = e.X; 
+            y = e.Y;
+        }
+    }
+}
